30679295472df3a0f5f9a270794c7a08b49d25c5
Nocetti Lago, Nicolás <nnocetti@antel.com.uy>
2021-04-28 04:06:29 -0300

    ...

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,23 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+var CollisionMode;
+(function (CollisionMode) {
+    CollisionMode[CollisionMode["merge"] = 0] = "merge";
+    CollisionMode[CollisionMode["bounce"] = 1] = "bounce";
+    CollisionMode[CollisionMode["overlap"] = 2] = "overlap";
+})(CollisionMode || (CollisionMode = {}));
+var EdgeMode;
+(function (EdgeMode) {
+    EdgeMode[EdgeMode["bounce"] = 0] = "bounce";
+    EdgeMode[EdgeMode["delete"] = 1] = "delete";
+    EdgeMode[EdgeMode["relocate"] = 2] = "relocate";
+})(EdgeMode || (EdgeMode = {}));
 var goldenAngle = 180 * (3 - Math.sqrt(5));
 var spaceWidth = 200;
 var spaceHeight = 200;
 var spaceDepth = 200;
 var constantG = 1;
+var collisionMode = CollisionMode.overlap;
 var Program = /** @class */ (function () {
     function Program() {
     }
@@ -16,6 +29,9 @@ var Program = /** @class */ (function () {
         document.body.appendChild(this.renderer.domElement);
         this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.camera.position.x = spaceWidth / 2;
+        this.camera.position.y = spaceHeight / 2;
+        this.camera.position.z = spaceDepth + 100;
     };
     Program.Main = function () {
         var that = this;
@@ -24,26 +40,38 @@ var Program = /** @class */ (function () {
         this.scene.add(space);
         var light = this.makeLight();
         this.scene.add(light);
-        this.camera.position.x = spaceWidth / 2;
-        this.camera.position.y = spaceHeight / 2;
-        this.camera.position.z = spaceDepth + 100;
         var obj;
         var position;
         var color;
         var radio;
-        for (var i = 0; i < 300; i++) {
+        for (var i = 0; i < 50; i++) {
             position = {
                 x: Math.floor(Math.random() * spaceWidth),
                 y: Math.floor(Math.random() * spaceHeight),
                 z: Math.floor(Math.random() * spaceDepth)
             };
             color = "hsl(" + (i * goldenAngle + 50) + ", 100%, 75%)";
-            radio = Math.ceil(Math.random() * 5);
+            radio = Math.random() * 2;
             obj = new Object(position, color, radio, radio);
+            obj.velocity = {
+                x: Math.random() * 3 * (Math.round(Math.random()) ? 1 : -1),
+                y: Math.random() * 3 * (Math.round(Math.random()) ? 1 : -1),
+                z: Math.random() * 3 * (Math.round(Math.random()) ? 1 : -1)
+            };
             this.scene.add(obj.figure);
             this.objects.push(obj);
             //console.log('positon: ', obj.position);
             //console.log('velocity: ', obj.velocity);
+            var points = [];
+            var vPosition = new THREE.Vector3(position.x, position.y, position.z);
+            for (var j = 0; j < 1000; j++) {
+                points[j] = vPosition.clone();
+            }
+            var lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+            var lineMaterial = new THREE.LineBasicMaterial({ color: color });
+            var trail = new THREE.Line(lineGeometry, lineMaterial);
+            this.trails.push(trail);
+            this.scene.add(trail);
         }
         requestAnimationFrame(this.timeFrame.bind(this));
     };
@@ -61,14 +89,32 @@ var Program = /** @class */ (function () {
                 if (i != j) {
                     if (!gForces["" + j + i]) {
                         var jObj = this.objects[j];
+                        // Calculamos la distancia entre los centros de los objetos
                         var dx = jObj.position.x - iObj.position.x;
                         var dy = jObj.position.y - iObj.position.y;
                         var dz = jObj.position.z - iObj.position.z;
+                        // Este resultado parcial de la distancia me sirve en la formula que calcula la magnitud de
+                        // la fuerza gravitacional
+                        // Es el cuadrado de la distancia entre los centros de los objetos
                         var r2 = (Math.pow(dx, 2) +
                             Math.pow(dy, 2) +
                             Math.pow(dz, 2));
+                        // Distancia entre el centro de los objetos
+                        var objDist = Math.pow(r2, 0.5);
+                        // Magnitud de la fuerza gravitacional
                         var forceMag = (constantG * this.objects[i].mass * this.objects[j].mass) / r2;
-                        // Este resultado hay que guardarlo, es el mismo para ij y para ji
+                        // Si la distancia entre el centro de los objetos es menor que la suma de los radios,
+                        // entonces empezaron a solaparse!
+                        // Calculamos la fuerza actuante con una regla de tres,
+                        // talvez no sea exacto pero se debe aproximar
+                        var rSum = iObj.radio + jObj.radio;
+                        if (objDist <= rSum) {
+                            if (collisionMode == CollisionMode.overlap) {
+                                var actingForce = objDist / rSum;
+                                forceMag *= actingForce;
+                            }
+                        }
+                        // Este resultado hay que guardarlo, porque ijForce = (-1)jiForce
                         var ijForce = { x: dx * forceMag, y: dy * forceMag, z: dz * forceMag };
                         totalForce.x += ijForce.x;
                         totalForce.y += ijForce.y;
@@ -103,6 +149,16 @@ var Program = /** @class */ (function () {
             iObj.position = newPosition;
             iObj.velocity = newVelocity;
             iObj.updateFigure();
+            // Trails
+            var trailPositions = this.trails[i].geometry.attributes.position.array;
+            for (var j = trailPositions.length - 1; j >= 3; j--) {
+                trailPositions[j] = trailPositions[j - 3];
+            }
+            trailPositions[0] = newPosition.x;
+            trailPositions[1] = newPosition.y;
+            trailPositions[2] = newPosition.z;
+            this.trails[i].geometry.attributes.position.needsUpdate = true;
+            //console.log('positions: ', this.trails[i].geometry.attributes.position.array)
         }
         requestAnimationFrame(this.timeFrame.bind(this));
     };
@@ -124,20 +180,9 @@ var Program = /** @class */ (function () {
     };
     Program.elapsedTime = 0;
     Program.objects = [];
+    Program.trails = [];
     return Program;
 }());
-var CollisionMode;
-(function (CollisionMode) {
-    CollisionMode[CollisionMode["merge"] = 0] = "merge";
-    CollisionMode[CollisionMode["bounce"] = 1] = "bounce";
-    CollisionMode[CollisionMode["overlap"] = 2] = "overlap";
-})(CollisionMode || (CollisionMode = {}));
-var EdgeMode;
-(function (EdgeMode) {
-    EdgeMode[EdgeMode["bounce"] = 0] = "bounce";
-    EdgeMode[EdgeMode["delete"] = 1] = "delete";
-    EdgeMode[EdgeMode["relocate"] = 2] = "relocate";
-})(EdgeMode || (EdgeMode = {}));
 var Object = /** @class */ (function () {
     function Object(position, color, radio, mass) {
         if (color === void 0) { color = 0xFFFFFF; }
@@ -147,18 +192,13 @@ var Object = /** @class */ (function () {
         this.color = color;
         this.radio = radio;
         this.mass = mass;
-        this.collisionMode = CollisionMode.overlap;
+        this.velocity = { x: 0, y: 0, z: 0 };
         this.figure = this.makeFigure();
-        this.velocity = {
-            x: Math.random() / 100 * (Math.round(Math.random()) ? 1 : -1),
-            y: Math.random() / 100 * (Math.round(Math.random()) ? 1 : -1),
-            z: Math.random() / 100 * (Math.round(Math.random()) ? 1 : -1)
-        };
     }
     Object.prototype.makeFigure = function () {
         var geometry = new THREE.SphereGeometry(this.radio);
         var material;
-        if (this.collisionMode == CollisionMode.overlap) {
+        if (collisionMode == CollisionMode.overlap) {
             material = new THREE.MeshPhongMaterial({ color: this.color, transparent: true, opacity: 0.65 });
         }
         else {
@@ -179,13 +219,6 @@ var Object = /** @class */ (function () {
             newPosition = position - module;
         }
         return [newPosition, velocity];
-    };
-    Object.prototype.move = function (dTime) {
-        var _a, _b, _c;
-        _a = this.moveAxis(this.position.x, this.velocity.x, dTime, spaceWidth), this.position.x = _a[0], this.velocity.x = _a[1];
-        _b = this.moveAxis(this.position.y, this.velocity.y, dTime, spaceHeight), this.position.y = _b[0], this.velocity.y = _b[1];
-        _c = this.moveAxis(this.position.z, this.velocity.z, dTime, spaceDepth), this.position.z = _c[0], this.velocity.z = _c[1];
-        this.updateFigure();
     };
     Object.prototype.updateFigure = function () {
         this.figure.position.x = this.position.x;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
 import * as THREE from 'three';
-import { Camera, TetrahedronBufferGeometry } from 'three';
+import { Line } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+//import { GUI } from 'dat.gui';
+//import { Points } from 'three';
+
+interface Vector {
+    x: number;
+    y: number;
+    z: number;
+}
+
+enum CollisionMode {
+    merge,
+    bounce,
+    overlap
+}
+
+enum EdgeMode {
+    bounce,
+    delete,
+    relocate
+}
 
 const goldenAngle = 180 * (3 - Math.sqrt(5));
 
@@ -9,6 +30,8 @@ const spaceHeight = 200;
 const spaceDepth = 200;
 
 const constantG = 1;
+
+const collisionMode = CollisionMode.overlap;
 
 class Program
 {
@@ -19,6 +42,7 @@ class Program
 
     static elapsedTime: number = 0;
     static objects: Object[] = [];
+    static trails: any[] = [];
 
     public static init(): void {
         this.scene = new THREE.Scene();
@@ -26,10 +50,15 @@ class Program
 
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
+
         document.body.appendChild(this.renderer.domElement);
 
         this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.camera.position.x = spaceWidth/2;
+        this.camera.position.y = spaceHeight/2;
+        this.camera.position.z = spaceDepth + 100;
+
     }
 
     public static Main(): void
@@ -44,16 +73,12 @@ class Program
         let light = this.makeLight();
         this.scene.add(light);
 
-        this.camera.position.x = spaceWidth/2;
-        this.camera.position.y = spaceHeight/2;
-        this.camera.position.z = spaceDepth + 100;
-
         let obj: Object;
         let position: Vector;
         let color: string;
         let radio: number;
 
-        for (let i=0; i<300; i++) {
+        for (let i=0; i<50; i++) {
             position = {
                 x: Math.floor(Math.random() * spaceWidth),
                 y: Math.floor(Math.random() * spaceHeight),
@@ -61,15 +86,37 @@ class Program
             };
             color = `hsl(${i * goldenAngle + 50}, 100%, 75%)`;
 
-            radio = Math.ceil(Math.random() * 5);
+            radio = Math.random() * 2;
 
             obj = new Object(position, color, radio, radio);
+
+            obj.velocity = {
+                x: Math.random() * 3 * (Math.round(Math.random()) ? 1 : -1),
+                y: Math.random() * 3 * (Math.round(Math.random()) ? 1 : -1),
+                z: Math.random() * 3 * (Math.round(Math.random()) ? 1 : -1)
+            }
 
             this.scene.add(obj.figure);
 
             this.objects.push(obj);
+
             //console.log('positon: ', obj.position);
             //console.log('velocity: ', obj.velocity);
+
+            let points: THREE.Vector3[] = [];
+            let vPosition = new THREE.Vector3(position.x, position.y, position.z);
+        for (let j = 0; j < 1000; j++) {
+                points[j] = vPosition.clone(); 
+            }
+            let lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+
+            let lineMaterial = new THREE.LineBasicMaterial({color: color});
+
+            let trail  = new THREE.Line(lineGeometry, lineMaterial)
+
+            this.trails.push(trail);
+
+            this.scene.add(trail)
         }
 
         requestAnimationFrame(this.timeFrame.bind(this));
@@ -95,19 +142,39 @@ class Program
                     if (!gForces[`${j}${i}`]) {
                         let jObj = this.objects[j];
 
+                        // Calculamos la distancia entre los centros de los objetos
                         let dx = jObj.position.x - iObj.position.x;
                         let dy = jObj.position.y - iObj.position.y;
                         let dz = jObj.position.z - iObj.position.z;
-    
+
+                        // Este resultado parcial de la distancia me sirve en la formula que calcula la magnitud de
+                        // la fuerza gravitacional
+                        // Es el cuadrado de la distancia entre los centros de los objetos
                         let r2 = (
                             Math.pow(dx, 2) +
                             Math.pow(dy, 2) +
                             Math.pow(dz, 2)
                         );
-    
+
+                        // Distancia entre el centro de los objetos
+                        let objDist = Math.pow(r2, 0.5);
+
+                        // Magnitud de la fuerza gravitacional
                         let forceMag = (constantG*this.objects[i].mass*this.objects[j].mass)/r2;
-    
-                        // Este resultado hay que guardarlo, es el mismo para ij y para ji
+
+                        // Si la distancia entre el centro de los objetos es menor que la suma de los radios,
+                        // entonces empezaron a solaparse!
+                        // Calculamos la fuerza actuante con una regla de tres,
+                        // talvez no sea exacto pero se debe aproximar
+                        let rSum = iObj.radio + jObj.radio;
+                        if (objDist <= rSum) {
+                            if (collisionMode == CollisionMode.overlap) {
+                                let actingForce = objDist / rSum;
+                                forceMag *= actingForce;
+                            }
+                        }
+
+                        // Este resultado hay que guardarlo, porque ijForce = (-1)jiForce
                         let ijForce = {x: dx*forceMag, y: dy*forceMag, z: dz*forceMag}
 
                         totalForce.x += ijForce.x;
@@ -152,6 +219,22 @@ class Program
             iObj.velocity = newVelocity;
 
             iObj.updateFigure();
+
+
+            // Trails
+
+            const trailPositions = this.trails[i].geometry.attributes.position.array;
+
+            for (let j = trailPositions.length - 1; j >= 3; j--) {
+                trailPositions[j] = trailPositions[j-3];
+            }
+            trailPositions[0] = newPosition.x;
+            trailPositions[1] = newPosition.y;
+            trailPositions[2] = newPosition.z;
+
+            this.trails[i].geometry.attributes.position.needsUpdate = true;
+
+            //console.log('positions: ', this.trails[i].geometry.attributes.position.array)
         }
 
         requestAnimationFrame(this.timeFrame.bind(this));
@@ -179,31 +262,11 @@ class Program
     }
 }
 
-interface Vector {
-    x: number;
-    y: number;
-    z: number;
-}
-
-enum CollisionMode {
-    merge,
-    bounce,
-    overlap
-}
-
-enum EdgeMode {
-    bounce,
-    delete,
-    relocate
-}
-
 class Object {
     position: Vector;
     color: number | string;
     radio: number;
     mass: number;
-
-    collisionMode: CollisionMode;
 
     velocity: Vector;
 
@@ -215,22 +278,16 @@ class Object {
         this.radio = radio;
         this.mass = mass;
 
-        this.collisionMode = CollisionMode.overlap;
+        this.velocity = {x:0, y:0, z:0};
 
         this.figure = this.makeFigure();
-
-        this.velocity = {
-            x: Math.random()/100 * (Math.round(Math.random()) ? 1 : -1),
-            y: Math.random()/100 * (Math.round(Math.random()) ? 1 : -1),
-            z: Math.random()/100 * (Math.round(Math.random()) ? 1 : -1)
-        }
     }
 
     private makeFigure(): THREE.Mesh {
         let geometry = new THREE.SphereGeometry(this.radio);
 
         let material: THREE.MeshPhongMaterial;
-        if (this.collisionMode == CollisionMode.overlap) {
+        if (collisionMode == CollisionMode.overlap) {
             material = new THREE.MeshPhongMaterial({ color: this.color, transparent: true, opacity: 0.65 });
         }
         else {
@@ -259,14 +316,6 @@ class Object {
         }
 
         return [newPosition, velocity];
-    }
-
-    move(dTime: number) {
-        [this.position.x, this.velocity.x] = this.moveAxis(this.position.x, this.velocity.x, dTime, spaceWidth);
-        [this.position.y, this.velocity.y] = this.moveAxis(this.position.y, this.velocity.y, dTime, spaceHeight);
-        [this.position.z, this.velocity.z] = this.moveAxis(this.position.z, this.velocity.z, dTime, spaceDepth);
-
-        this.updateFigure();
     }
 
     updateFigure(): void {
